5d422683d75e97d65ee52c29bcc705facad1015a
anoopna <anoopks1207@gmail.com>
2023-09-18 22:40:10 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,34 +7,34 @@ terraform {
 
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
-  #user_ocid        = var.user_ocid
-  #fingerprint      = var.fingerprint
-  #private_key_path = var.private_key_path
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
   region           = var.region
 }
 
-#provider "oci" {
-#  alias                = "homeregion"
-#  tenancy_ocid         = var.tenancy_ocid
-#  user_ocid            = var.user_ocid
-#  fingerprint          = var.fingerprint
-#  private_key_path     = var.private_key_path
-#  region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
-#  disable_auto_retries = "true"
-#}
+provider "oci" {
+  alias                = "homeregion"
+  tenancy_ocid         = var.tenancy_ocid
+  user_ocid            = var.user_ocid
+  fingerprint          = var.fingerprint
+  private_key_path     = var.private_key_path
+  region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
+  disable_auto_retries = "true"
+}
 
-#provider "oci" {
-#  alias                = "targetregion"
-#  tenancy_ocid         = var.tenancy_ocid
-#  user_ocid            = var.user_ocid
-#  fingerprint          = var.fingerprint
-#  private_key_path     = var.private_key_path
-#  region               = var.region
-#  disable_auto_retries = "true"
-#}
+provider "oci" {
+  alias                = "targetregion"
+  tenancy_ocid         = var.tenancy_ocid
+  user_ocid            = var.user_ocid
+  fingerprint          = var.fingerprint
+  private_key_path     = var.private_key_path
+  region               = var.region
+  disable_auto_retries = "true"
+}
 
 # Variables required by the OCI Provider only when running Terraform CLI with standard user based Authentication
-#variable "user_ocid" {}
-#variable "fingerprint" {}
-#variable "private_key_path" {}
+variable "user_ocid" { ocid1.user.oc1..aaaaaaaaedytf24fnrvlgu562epmhkc3t27dmtv5zrodnhjit6e3l2obujdq }
+variable "fingerprint" { a7:b6:c9:77:72:c1:37:8d:4f:5d:a4:5d:3c:11:cf:61 }
+variable "private_key_path" {/home}
 
