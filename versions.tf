40f2305af69f2bf323c8b399ce593d1f96306040
anoopna <anoopks1207@gmail.com>
2023-09-18 20:18:37 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,7 +1,11 @@
 ## Copyright © 2021, Oracle and/or its affiliates. 
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
-
 terraform {
   required_version = "> 0.12"
+  required_providers {
+     oci = {
+         source = "oracle/oci"
+         version = "5.1.0"
+     }
+  }
 }
-
