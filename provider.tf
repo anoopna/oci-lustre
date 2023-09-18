9c271570dbd9e3e0946f9a8b056c1ea2e576e5b5
anoopna <anoopks1207@gmail.com>
2023-09-18 20:29:34 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,31 +7,31 @@ terraform {
 
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
+  #user_ocid        = var.user_ocid
+  #fingerprint      = var.fingerprint
+  #private_key_path = var.private_key_path
   region           = var.region
 }
 
-provider "oci" {
-  alias                = "homeregion"
-  tenancy_ocid         = var.tenancy_ocid
-  user_ocid            = var.user_ocid
-  fingerprint          = var.fingerprint
-  private_key_path     = var.private_key_path
-  region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
-  disable_auto_retries = "true"
-}
+#provider "oci" {
+#  alias                = "homeregion"
+#  tenancy_ocid         = var.tenancy_ocid
+#  user_ocid            = var.user_ocid
+#  fingerprint          = var.fingerprint
+#  private_key_path     = var.private_key_path
+#  region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
+#  disable_auto_retries = "true"
+#}
 
-provider "oci" {
-  alias                = "targetregion"
-  tenancy_ocid         = var.tenancy_ocid
-  user_ocid            = var.user_ocid
-  fingerprint          = var.fingerprint
-  private_key_path     = var.private_key_path
-  region               = var.region
-  disable_auto_retries = "true"
-}
+#provider "oci" {
+#  alias                = "targetregion"
+#  tenancy_ocid         = var.tenancy_ocid
+#  user_ocid            = var.user_ocid
+#  fingerprint          = var.fingerprint
+#  private_key_path     = var.private_key_path
+#  region               = var.region
+#  disable_auto_retries = "true"
+#}
 
 # Variables required by the OCI Provider only when running Terraform CLI with standard user based Authentication
 #variable "user_ocid" {}
